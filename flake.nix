c5c9d831ea4175723472e7c14ae42dc4d1730a82
Ingo Ruhnke <grumbel@gmail.com>
2021-11-14 16:53:27 +0100

    Add Nix flake build file

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "An app that fills the whole screen with a color";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    nix.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nix, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+       in rec {
+        packages = flake-utils.lib.flattenTree rec {
+          qflashlight = pkgs.python3Packages.buildPythonPackage rec {
+            name = "qflashlight";
+            src = self;
+            nativeBuildInputs = [ pkgs.qt5.wrapQtAppsHook ];
+            makeWrapperArgs = [
+              "\${qtWrapperArgs[@]}"
+
+              "--set" "LIBGL_DRIVERS_PATH" "${pkgs.mesa.drivers}/lib/dri"
+              "--prefix" "LD_LIBRARY_PATH" ":" "${pkgs.mesa.drivers}/lib"
+            ];
+            preCheck = ''
+              export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins";
+            '';
+            propagatedBuildInputs = [
+              pkgs.python3Packages.setuptools
+              pkgs.python3Packages.pyqt5
+              pkgs.python3Packages.pyxdg
+            ];
+          };
+        };
+        defaultPackage = packages.qflashlight;
+      });
+}
