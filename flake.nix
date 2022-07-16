4cbaea20f1277bd99a82a166dd0ec95001c1c047
Ingo Ruhnke <grumbel@gmail.com>
2022-07-16 14:05:30 +0200

    Update flake.nix, add devShell, app and 'default = ...'

--- flake.nix
+++ flake.nix
@@ -10,9 +10,19 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        pythonPackages = pkgs.python310Packages;
        in rec {
-        packages = flake-utils.lib.flattenTree rec {
-          qflashlight = pkgs.python3Packages.buildPythonPackage rec {
+         packages = flake-utils.lib.flattenTree rec {
+           PyQt5-stubs = pythonPackages.buildPythonPackage rec {
+             pname = "PyQt5-stubs";
+             version = "5.15.6.0";
+             src = pythonPackages.fetchPypi {
+               inherit pname version;
+               sha256 = "sha256-kScKwj6/OKHcBM2XqoUs0Ir4Lcg5EA5Tla8UR+Pplwc=";
+             };
+           };
+
+          qflashlight = pythonPackages.buildPythonPackage rec {
             name = "qflashlight";
             src = self;
             nativeBuildInputs = [ pkgs.qt5.wrapQtAppsHook ];
@@ -25,13 +35,46 @@
             preCheck = ''
               export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins";
             '';
-            propagatedBuildInputs = [
-              pkgs.python3Packages.setuptools
-              pkgs.python3Packages.pyqt5
-              pkgs.python3Packages.pyxdg
+            propagatedBuildInputs = with pythonPackages; [
+              setuptools
+              pyqt5
+              pyxdg
+            ];
+            checkInputs = (with pkgs; [
+              pyright
+            ]) ++ (with pythonPackages; [
+              flake8
+              mypy
+              pylint
+              types-setuptools
+            ]) ++ [
+              PyQt5-stubs
             ];
           };
+
+          qflashlight-nocheck = qflashlight.override {
+            doCheck = false;
+          };
+
+          default = qflashlight;
         };
-        defaultPackage = packages.qflashlight;
-      });
+
+        devShells = rec {
+          qflashlight-dev = pkgs.mkShell {
+            inputsFrom = [ packages.qflashlight ];
+            shellHook = packages.qflashlight.preCheck + ''
+              # runHook setuptoolsShellHook
+            '';
+          };
+        };
+
+        apps = rec {
+          qflashlight = flake-utils.lib.mkApp {
+            drv = packages.qflashlight;
+            exePath = "/bin/qflashlight";
+          };
+          default = qflashlight;
+        };
+       }
+    );
 }
