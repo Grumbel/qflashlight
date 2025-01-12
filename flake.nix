e5eec1bc6ca7fa8da64c66740909807ce26c5bdd
Ingo Ruhnke <grumbel@gmail.com>
2025-01-12 18:59:31 +0100

    Update flake.nix to nixos-24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "An app that fills the whole screen with a color";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
